53dcef6d52c8ddcc3101e165c284af22f19886e5
ziaoang <ziaoang@gmail.com>
2017-10-08 00:30:27 +0800

    add click function

--- Assets/Scripts/Data/SharedData.cs
+++ Assets/Scripts/Data/SharedData.cs
@@ -1,8 +1,8 @@
 ﻿public static class SharedData
 {
 	public static int vitality = 0;
-	public static int vitalityPerClick = 10;
-	public static int vitalityPerSecond = 20;
+	public static int vitalityPerClick = 5;
+	public static int vitalityPerSecond = 10;
 }
 
 
---
+++ Assets/Scripts/Manager/TouchManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour {
+	
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			Messenger.Broadcast (GameEvent.Msg_Click);
+		}
+	}
+}
